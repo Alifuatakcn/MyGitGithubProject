e089cf11c2b486e411c9b09ee7be50d928eafa9c
Alifuatakcn <fakcan4908@gmail.com>
2023-04-26 23:02:27 +0300

    ali2

---
+++ src/main/java/Account.java
@@ -0,0 +1,18 @@
+public class Account {
+    public static void main(String[] args) {
+        System.out.println("Bu bir ATM projesidir");
+        //version1
+        System.out.println("Java is easy");
+        //version2
+        System.out.println("Learn Java Earn Money");
+        //version3
+        System.out.println("Git ögrenmek cok kolay");
+        //version4
+
+        System.out.println("ali  branch");
+        //version ali1
+
+        System.out.println("bir kısım kodlar");
+        //ali2
+    }
+}
