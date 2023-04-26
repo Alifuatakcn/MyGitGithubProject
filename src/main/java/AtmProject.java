d915e0fc1df0ebee5ae62eac996680c11c8cc71c
Alifuatakcn <fakcan4908@gmail.com>
2023-04-26 22:03:14 +0300

    v4

--- src/main/java/AtmProject.java
+++ src/main/java/AtmProject.java
@@ -6,5 +6,7 @@ public class AtmProject {
         //version2
         System.out.println("Learn Java Earn Money");
         //version3
+        System.out.println("Git ögrenmek cok kolay");
+        //version4
     }
 }
